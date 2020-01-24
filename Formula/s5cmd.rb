6508cf30706f84529db5e67e4a7e3046d17376e0
Onur Sonmez <onursonmez@peak.com>
2020-01-24 15:49:07 +0300

    Update brew formula

--- Formula/s5cmd.rb
+++ Formula/s5cmd.rb
@@ -1,12 +1,10 @@
-# Classname should match the name of the installed package.
 class S5cmd < Formula
   desc "Parallel S3 and local filesystem execution tool"
   homepage "https://github.com/peak/s5cmd"
 
   # Source code archive. Each tagged release will have one
-  url "https://github.com/peak/s5cmd/archive/v0.6.0.tar.gz"
-  sha256 "70d89cc5f4d5c26aff6af716e3a6bb5d17bc084fa09c89312ac4eb69ae468a2a"
-  head "https://github.com/peak/s5cmd"
+  url "https://github.com/peak/s5cmd/archive/v0.6.2.tar.gz"
+  sha256 "c38cbe80465ff0ccde95a2bbdbcd2bcd8f0326d074d7f8e4a993d586dca28ef8"
 
   depends_on "go" => :build
 
@@ -18,7 +16,7 @@ class S5cmd < Formula
     # to $GOPATH/src/github.com/peak/s3hash
     bin_path.install Dir["*"]
     cd bin_path do
-      # Install the compiled binary into Homebrew's `bin` - a pre-existing
+      # Install the compiled binary into Homebrew's `bin` - a pre-exisxting
       # global variable
       system "go", "build", "-o", bin/"s5cmd"
     end
@@ -26,9 +24,7 @@ class S5cmd < Formula
     ohai "To install shell completion, run s5cmd -cmp-install"
   end
 
-  # Homebrew requires tests.
   test do
-    # "2>&1" redirects standard error to stdout.
-    assert_match "s5cmd version v0.6.0", shell_output("#{bin}/s5cmd -version 2>&1", 0)
+    assert_match "s5cmd version v0.6.2", shell_output("#{bin}/s5cmd -version 2>&1", 0)
   end
 end

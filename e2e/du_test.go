04cc10e7a6ba742f769bf43e8feb802a6f008292
İbrahim Güngör <igungor@gmail.com>
2020-01-31 14:23:08 +0300

    e2e: complete 'du' tests

--- e2e/du_test.go
+++ e2e/du_test.go
@@ -1,6 +1,7 @@
 package e2e
 
 import (
+	"strings"
 	"testing"
 
 	"gotest.tools/v3/icmd"
@@ -28,12 +29,123 @@ func TestDiskUsageSingleS3Object(t *testing.T) {
 	}, strictLineCheck(false))
 
 	assertLines(t, result.Stdout(), map[int]compareFunc{
-		// 0: suffix("317 testfile1.txt"),
+		0: suffix(` + 317 bytes in 1 objects: s3://%v/testfile1.txt [Total]`, bucket),
 	})
 }
 
-func TestDiskUsageWildcardS3Objects(t *testing.T) {
+func TestDiskUsageMultipleS3Objects(t *testing.T) {
+	bucket := s3BucketFromTestName(t)
+
+	s3client, s5cmd, cleanup := setup(t)
+	defer cleanup()
+
+	createBucket(t, s3client, bucket)
+
+	putFile(t, s3client, bucket, "testfile1.txt", "this is a file content")
+	putFile(t, s3client, bucket, "testfile2.txt", "this is also a file content")
+	putFile(t, s3client, bucket, "foo/testfile3.txt", "this is also a file content")
+
+	cmd := s5cmd("du", "s3://"+bucket)
+	result := icmd.RunCmd(cmd)
+
+	result.Assert(t, icmd.Success)
+
+	assertLines(t, result.Stderr(), map[int]compareFunc{
+		0: suffix(` +OK "du s3://%v" (1)`, bucket),
+	}, strictLineCheck(false))
+
+	assertLines(t, result.Stdout(), map[int]compareFunc{
+		0: suffix(` + 639 bytes in 2 objects: s3://%v/ [Total]`, bucket),
+	})
+}
+
+func TestDiskUsageWildcard(t *testing.T) {
+	bucket := s3BucketFromTestName(t)
+
+	s3client, s5cmd, cleanup := setup(t)
+	defer cleanup()
+
+	createBucket(t, s3client, bucket)
+	putFile(t, s3client, bucket, "testfile1.txt", "this is a file content")
+	putFile(t, s3client, bucket, "testfile2.txt", "this is also a file content")
+	putFile(t, s3client, bucket, "foo/testfile3.txt", "this is also a file content somehow")
+	putFile(t, s3client, bucket, "bar/testfile3.gz", "this is also a file content somehow")
+
+	cmd := s5cmd("du", "s3://"+bucket+"/*.txt")
+	result := icmd.RunCmd(cmd)
+
+	result.Assert(t, icmd.Success)
+
+	assertLines(t, result.Stderr(), map[int]compareFunc{
+		0: suffix(` +OK "du s3://%v/*.txt" (1)`, bucket),
+	}, strictLineCheck(false))
+
+	assertLines(t, result.Stdout(), map[int]compareFunc{
+		0: suffix(` + 973 bytes in 3 objects: s3://%v/*.txt [Total]`, bucket),
+	})
+}
+
+func TestDiskUsageS3ObjectsAndFolders(t *testing.T) {
+	bucket := s3BucketFromTestName(t)
+
+	s3client, s5cmd, cleanup := setup(t)
+	defer cleanup()
+
+	createBucket(t, s3client, bucket)
+	putFile(t, s3client, bucket, "testfile1.txt", "content")
+	putFile(t, s3client, bucket, "report.gz", "content")
+	putFile(t, s3client, bucket, "/a/testfile2.txt", "content")
+	putFile(t, s3client, bucket, "/b/testfile3.txt", "content")
+	putFile(t, s3client, bucket, "/b/testfile4.txt", "content")
+	putFile(t, s3client, bucket, "/c/testfile5.gz", "content")
+	putFile(t, s3client, bucket, "/d/foo/bar/file7.txt", "content")
+	putFile(t, s3client, bucket, "/d/foo/bar/testfile8.txt", "content")
+	putFile(t, s3client, bucket, "/e/txt/testfile9.txt.gz", "content")
+	putFile(t, s3client, bucket, "/f/txt/testfile10.txt", "content")
+
+	cmd := s5cmd("du", "s3://"+bucket)
+	result := icmd.RunCmd(cmd)
+
+	result.Assert(t, icmd.Success)
+
+	assertLines(t, result.Stderr(), map[int]compareFunc{
+		0: suffix(` +OK "ls s3://%v" (8)`, bucket),
+	}, strictLineCheck(false))
+
+	assertLines(t, result.Stdout(), map[int]compareFunc{
+		0: suffix("+ DIR a/"),
+		1: suffix("+ DIR b/"),
+		2: suffix("+ DIR c/"),
+		3: suffix("+ DIR d/"),
+		4: suffix("+ DIR e/"),
+		5: suffix("+ DIR f/"),
+		6: suffix("? 298 report.gz"),
+		7: suffix("? 302 testfile1.txt"),
+	})
 }
 
 func TestDiskUsageWildcardS3ObjectsWithDashH(t *testing.T) {
+	bucket := s3BucketFromTestName(t)
+
+	s3client, s5cmd, cleanup := setup(t)
+	defer cleanup()
+
+	createBucket(t, s3client, bucket)
+
+	putFile(t, s3client, bucket, "testfile1.txt", strings.Repeat("this is a file content", 10000))
+	putFile(t, s3client, bucket, "testfile2.txt", strings.Repeat("this is also a file content", 1000))
+	putFile(t, s3client, bucket, "foo/testfile3.txt", strings.Repeat("this is also a file content", 1000))
+
+	cmd := s5cmd("du", "-H", "s3://"+bucket)
+	result := icmd.RunCmd(cmd)
+
+	result.Assert(t, icmd.Success)
+
+	assertLines(t, result.Stderr(), map[int]compareFunc{
+		0: suffix(` +OK "du s3://%v" (1)`, bucket),
+	}, strictLineCheck(false))
+
+	assertLines(t, result.Stdout(), map[int]compareFunc{
+		0: suffix(` + 241.8K bytes in 2 objects: s3://%v/ [Total]`, bucket),
+	})
 }

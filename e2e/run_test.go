22dd92e832e60dd3de9c62dac210308bd294a396
brendan-matroid <brendan.graham@matroid.com>
2020-04-12 16:11:49 -0400

    Make testcase consistent with other cases

--- e2e/run_test.go
+++ e2e/run_test.go
@@ -225,11 +225,8 @@ func TestRunSpecialCharactersInPrefix(t *testing.T) {
 	result.Assert(t, icmd.Success)
 
 	assertLines(t, result.Stdout(), map[int]compareFunc{
-		0: equals(""),
-		1: equals(`cp s3://` + bucket + `/special-chars_!@#$%^&_()_+{[_%5Cäè| __;'_,_._-中文 =/_!@#$%^&_()_+{[_%5Cäè| __;'_,_._-中文 =image.jpg`),
+		0: equals(`cp s3://` + bucket + `/special-chars_!@#$%^&_()_+{[_%5Cäè| __;'_,_._-中文 =/_!@#$%^&_()_+{[_%5Cäè| __;'_,_._-中文 =image.jpg`),
 	}, sortInput(true))
 
-	assertLines(t, result.Stderr(), map[int]compareFunc{
-		0: equals(""),
-	}, strictLineCheck(true))
+	assertLines(t, result.Stderr(), map[int]compareFunc{})
 }

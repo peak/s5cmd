520203a479aa9284460c5a61f446e83a77695859
Onur Sonmez <onursonmez@peak.com>
2020-04-17 11:59:34 +0300

    Fix golint and misspell warnings

--- parallel/parallel.go
+++ parallel/parallel.go
@@ -11,22 +11,30 @@ const minOpenFilesLimit = 1000
 
 var global *Manager
 
+// Task is a function type for parallel manager.
 type Task func() error
 
+// Init tries to increase the soft limit of open files and
+// creates new global ParallelManager.
 func Init(workercount int) {
 	adjustOpenFilesLimit()
 	global = New(workercount)
 }
 
+// Close waits all jobs to finish and
+// closes the semaphore of global ParallelManager.
 func Close() { global.Close() }
 
+// Run runs global ParallelManager.
 func Run(task Task, waiter *Waiter) { global.Run(task, waiter) }
 
+// Manager is a structure for running tasks in parallel.
 type Manager struct {
 	wg        *sync.WaitGroup
 	semaphore chan bool
 }
 
+// New creates a new parallel.Manager.
 func New(workercount int) *Manager {
 	if workercount < 0 {
 		workercount = runtime.NumCPU() * -workercount
@@ -74,22 +82,28 @@ func (p *Manager) Close() {
 	close(p.semaphore)
 }
 
+// Waiter is a structure for waiting and reading
+// error messages created by Manager.
 type Waiter struct {
 	wg    sync.WaitGroup
 	errch chan error
 }
 
+// NewWaiter creates a new parallel.Waiter.
 func NewWaiter() *Waiter {
 	return &Waiter{
 		errch: make(chan error),
 	}
 }
 
+// Waits blocks until the WaitGroup counter is zero
+// and closes error channel.
 func (w *Waiter) Wait() {
 	w.wg.Wait()
 	close(w.errch)
 }
 
+// Err returns read-only error channel.
 func (w *Waiter) Err() <-chan error {
 	return w.errch
 }

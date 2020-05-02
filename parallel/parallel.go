1f7ff15ed08b1a550f45c2172e4af5101c82916c
İbrahim Güngör <igungor@gmail.com>
2020-05-02 13:26:25 +0300

    parallel: move global manager into its own file

--- parallel/parallel.go
+++ parallel/parallel.go
@@ -3,30 +3,15 @@ package parallel
 import (
 	"runtime"
 	"sync"
-	"syscall"
 )
 
-const minNumWorkers = 2
-const minOpenFilesLimit = 1000
-
-var global *Manager
+const (
+	minNumWorkers     = 2
+	minOpenFilesLimit = 1000
+)
 
 // Task is a function type for parallel manager.
 type Task func() error
-
-// Init tries to increase the soft limit of open files and
-// creates new global ParallelManager.
-func Init(workercount int) {
-	adjustOpenFilesLimit()
-	global = New(workercount)
-}
-
-// Close waits all jobs to finish and
-// closes the semaphore of global ParallelManager.
-func Close() { global.Close() }
-
-// Run runs global ParallelManager.
-func Run(task Task, waiter *Waiter) { global.Run(task, waiter) }
 
 // Manager is a structure for running tasks in parallel.
 type Manager struct {
@@ -106,25 +91,4 @@ func (w *Waiter) Wait() {
 // Err returns read-only error channel.
 func (w *Waiter) Err() <-chan error {
 	return w.errch
-}
-
-// adjustOpenFilesLimit tries to increase the soft limit of open files.
-func adjustOpenFilesLimit() {
-	var rLimit syscall.Rlimit
-	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
-	if err != nil {
-		return
-	}
-
-	if rLimit.Cur >= minOpenFilesLimit {
-		return
-	}
-
-	if rLimit.Max < minOpenFilesLimit {
-		return
-	}
-
-	rLimit.Cur = minOpenFilesLimit
-
-	_ = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
 }

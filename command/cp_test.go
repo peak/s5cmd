5050ee12818b3977c0c0368ba6b189331398335e
Farukhzhon <fbarotov@sabanciuniv.edu>
2020-08-25 16:58:26 +0300

    command/cp: fix mime type inference

---
+++ command/cp_test.go
@@ -0,0 +1,62 @@
+package command
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGuessContentType(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		filename string
+		content  string
+
+		expectedContentType string
+	}{
+		{
+			filename:            "*.pdf",
+			expectedContentType: "application/pdf",
+		},
+		{
+			filename:            "*.css",
+			expectedContentType: "text/css; charset=utf-8",
+		},
+		{
+			filename: "index",
+			content: `
+					<!DOCTYPE html>
+					<html>
+						<head>
+							<title>Hello World</title>
+						</head>
+						<body>
+							<p>Hello, World! I am s5cmd :)</p>
+						</body>
+					</html>
+					`,
+			expectedContentType: "text/plain; charset=utf-8",
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc
+
+		f, err := ioutil.TempFile("", tc.filename)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if tc.content != "" {
+			f.WriteString(tc.content)
+		}
+
+		assert.Equal(t, tc.expectedContentType, guessContentType(f))
+
+		f.Close()
+		os.Remove(f.Name())
+	}
+}

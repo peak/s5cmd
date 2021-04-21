431ca90463d65dc2d32c03214bdf75bfbe2400fc
khacminh <khacminhrt@gmail.com>
2021-04-22 00:09:52 +0700

    revert go.mod

--- vendor/golang.org/x/sys/unix/ztypes_openbsd_386.go
+++ vendor/golang.org/x/sys/unix/ztypes_openbsd_386.go
@@ -231,7 +231,6 @@ const (
 	SizeofSockaddrUnix     = 0x6a
 	SizeofSockaddrDatalink = 0x20
 	SizeofLinger           = 0x8
-	SizeofIovec            = 0x8
 	SizeofIPMreq           = 0x8
 	SizeofIPv6Mreq         = 0x14
 	SizeofMsghdr           = 0x1c
@@ -437,7 +436,6 @@ type Winsize struct {
 
 const (
 	AT_FDCWD            = -0x64
-	AT_SYMLINK_FOLLOW   = 0x4
 	AT_SYMLINK_NOFOLLOW = 0x2
 )
 
--- vendor/golang.org/x/sys/unix/ztypes_solaris_amd64.go
+++ vendor/golang.org/x/sys/unix/ztypes_solaris_amd64.go
@@ -88,6 +88,7 @@ type Stat_t struct {
 	Mtim    Timespec
 	Ctim    Timespec
 	Blksize int32
+	_       [4]byte
 	Blocks  int64
 	Fstype  [16]int8
 }
@@ -95,6 +96,7 @@ type Stat_t struct {
 type Flock_t struct {
 	Type   int16
 	Whence int16
+	_      [4]byte
 	Start  int64
 	Len    int64
 	Sysid  int32
@@ -136,12 +138,12 @@ type RawSockaddrInet4 struct {
 }
 
 type RawSockaddrInet6 struct {
-	Family   uint16
-	Port     uint16
-	Flowinfo uint32
-	Addr     [16]byte /* in6_addr */
-	Scope_id uint32
-	_        uint32
+	Family         uint16
+	Port           uint16
+	Flowinfo       uint32
+	Addr           [16]byte /* in6_addr */
+	Scope_id       uint32
+	X__sin6_src_id uint32
 }
 
 type RawSockaddrUnix struct {
@@ -194,8 +196,10 @@ type IPv6Mreq struct {
 type Msghdr struct {
 	Name         *byte
 	Namelen      uint32
+	_            [4]byte
 	Iov          *Iovec
 	Iovlen       int32
+	_            [4]byte
 	Accrights    *int8
 	Accrightslen int32
 	_            [4]byte
@@ -205,12 +209,6 @@ type Cmsghdr struct {
 	Len   uint32
 	Level int32
 	Type  int32
-}
-
-type Inet4Pktinfo struct {
-	Ifindex  uint32
-	Spec_dst [4]byte /* in_addr */
-	Addr     [4]byte /* in_addr */
 }
 
 type Inet6Pktinfo struct {
@@ -224,7 +222,7 @@ type IPv6MTUInfo struct {
 }
 
 type ICMPv6Filter struct {
-	Filt [8]uint32
+	X__icmp6_filt [8]uint32
 }
 
 const (
@@ -234,12 +232,10 @@ const (
 	SizeofSockaddrUnix     = 0x6e
 	SizeofSockaddrDatalink = 0xfc
 	SizeofLinger           = 0x8
-	SizeofIovec            = 0x10
 	SizeofIPMreq           = 0x8
 	SizeofIPv6Mreq         = 0x14
 	SizeofMsghdr           = 0x30
 	SizeofCmsghdr          = 0xc
-	SizeofInet4Pktinfo     = 0xc
 	SizeofInet6Pktinfo     = 0x14
 	SizeofIPv6MTUInfo      = 0x24
 	SizeofICMPv6Filter     = 0x20
@@ -288,6 +284,7 @@ type IfMsghdr struct {
 	Addrs   int32
 	Flags   int32
 	Index   uint16
+	_       [2]byte
 	Data    IfData
 }
 
@@ -295,6 +292,7 @@ type IfData struct {
 	Type       uint8
 	Addrlen    uint8
 	Hdrlen     uint8
+	_          [1]byte
 	Mtu        uint32
 	Metric     uint32
 	Baudrate   uint32
@@ -319,6 +317,7 @@ type IfaMsghdr struct {
 	Addrs   int32
 	Flags   int32
 	Index   uint16
+	_       [2]byte
 	Metric  int32
 }
 
@@ -327,6 +326,7 @@ type RtMsghdr struct {
 	Version uint8
 	Type    uint8
 	Index   uint16
+	_       [2]byte
 	Flags   int32
 	Addrs   int32
 	Pid     int32
@@ -364,14 +364,15 @@ type BpfVersion struct {
 }
 
 type BpfStat struct {
-	Recv uint64
-	Drop uint64
-	Capt uint64
-	_    [13]uint64
+	Recv    uint64
+	Drop    uint64
+	Capt    uint64
+	Padding [13]uint64
 }
 
 type BpfProgram struct {
 	Len   uint32
+	_     [4]byte
 	Insns *BpfInsn
 }
 

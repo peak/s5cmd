3bac5b5474841f937320dc1db4829b5044fbadf1
Selman Kayrancioglu <selman@peak.com>
2021-07-29 16:13:36 +0300

    command,storage: gofmt

--- storage/storage.go
+++ storage/storage.go
@@ -48,13 +48,13 @@ func NewLocalClient(opts Options) *Filesystem {
 
 func NewRemoteClient(ctx context.Context, url *url.URL, opts Options) (*S3, error) {
 	newOpts := Options{
-		MaxRetries:  opts.MaxRetries,
-		Endpoint:    opts.Endpoint,
-		NoVerifySSL: opts.NoVerifySSL,
-		DryRun:      opts.DryRun,
+		MaxRetries:    opts.MaxRetries,
+		Endpoint:      opts.Endpoint,
+		NoVerifySSL:   opts.NoVerifySSL,
+		DryRun:        opts.DryRun,
 		NoSignRequest: opts.NoSignRequest,
-		bucket:      url.Bucket,
-		region:      opts.region,
+		bucket:        url.Bucket,
+		region:        opts.region,
 	}
 	return newS3Storage(ctx, newOpts)
 }
@@ -68,13 +68,13 @@ func NewClient(ctx context.Context, url *url.URL, opts Options) (Storage, error)
 
 // Options stores configuration for storage.
 type Options struct {
-	MaxRetries  int
-	Endpoint    string
-	NoVerifySSL bool
-	DryRun      bool
+	MaxRetries    int
+	Endpoint      string
+	NoVerifySSL   bool
+	DryRun        bool
 	NoSignRequest bool
-	bucket      string
-	region      string
+	bucket        string
+	region        string
 }
 
 func (o *Options) SetRegion(region string) {

f0f4861e36be09bbda0ac26a42e18e519beff1f2
Selman Kayrancioglu <selman@peak.com>
2021-09-29 18:44:11 +0300

    command/sync_strategy_test: add SizeOnlyStrategy test

--- command/sync_strategy_test.go
+++ command/sync_strategy_test.go
@@ -77,3 +77,73 @@ func TestSizeAndModificationStrategy_Compare(t *testing.T) {
 		})
 	}
 }
+
+func TestSizeOnlyStrategy_Compare(t *testing.T) {
+	ft := time.Now()
+	timePtr := func(tt time.Time) *time.Time {
+		return &tt
+	}
+	testcases := []struct {
+		name     string
+		src      *storage.Object
+		dst      *storage.Object
+		expected error
+	}{
+
+		{
+			//	time: src > dst       size: src != dst
+			name:     "source is newer, sizes are different",
+			src:      &storage.Object{ModTime: timePtr(ft.Add(time.Minute)), Size: 10},
+			dst:      &storage.Object{ModTime: timePtr(ft), Size: 5},
+			expected: nil,
+		},
+
+		{
+			//	time: src > dst       size: src = dst
+			name:     "source is newer, sizes are same",
+			src:      &storage.Object{ModTime: timePtr(ft.Add(time.Minute)), Size: 10},
+			dst:      &storage.Object{ModTime: timePtr(ft), Size: 10},
+			expected: errorpkg.ErrObjectSizesMatch,
+		},
+
+		{
+			//	time: src < dst       size: src != dst
+			name:     "source is older, sizes are different",
+			src:      &storage.Object{ModTime: timePtr(ft), Size: 10},
+			dst:      &storage.Object{ModTime: timePtr(ft.Add(time.Minute)), Size: 5},
+			expected: nil,
+		},
+
+		{
+			//	time: src < dst       size: src == dst
+			name:     "source is older, sizes are same",
+			src:      &storage.Object{ModTime: timePtr(ft), Size: 10},
+			dst:      &storage.Object{ModTime: timePtr(ft.Add(time.Minute)), Size: 10},
+			expected: errorpkg.ErrObjectSizesMatch,
+		},
+
+		{
+			//	time: src = dst       size: src != dst
+			name:     "files have same age, sizes are different",
+			src:      &storage.Object{ModTime: timePtr(ft), Size: 10},
+			dst:      &storage.Object{ModTime: timePtr(ft), Size: 5},
+			expected: nil,
+		},
+
+		{
+			//	time: src = dst       size: src == dst
+			name:     "files have same age, sizes are same",
+			src:      &storage.Object{ModTime: timePtr(ft), Size: 10},
+			dst:      &storage.Object{ModTime: timePtr(ft), Size: 10},
+			expected: errorpkg.ErrObjectSizesMatch,
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			strategy := &SizeOnlyStrategy{}
+			if got := strategy.Compare(tc.src, tc.dst); got != tc.expected {
+				t.Fatalf("expected: %q(%T), got: %q(%T)", tc.expected, tc.expected, got, got)
+			}
+		})
+	}
+}

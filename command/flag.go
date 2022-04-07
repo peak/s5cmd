96cbd15b6bc593d4f268c4e3f22bd6b628047915
Onur Sonmez <onursonmez@peak.com>
2022-04-07 16:50:09 +0300

    implement EnumValue for flag definition

---
+++ command/flag.go
@@ -0,0 +1,30 @@
+package command
+
+import (
+	"fmt"
+	"strings"
+)
+
+type EnumValue struct {
+	Enum     []string
+	Default  string
+	selected string
+}
+
+func (e *EnumValue) Set(value string) error {
+	for _, enum := range e.Enum {
+		if enum == value {
+			e.selected = value
+			return nil
+		}
+	}
+
+	return fmt.Errorf("allowed values: [%s]", strings.Join(e.Enum, ", "))
+}
+
+func (e EnumValue) String() string {
+	if e.selected == "" {
+		return e.Default
+	}
+	return e.selected
+}

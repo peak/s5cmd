7bd3355f36c1b7cce0a0352b63e54ffe23219643
MUHAMMED CAN KÜÇÜKASLAN <muhammedcankucukaslan@gmail.com>
2022-07-19 18:34:56 +0300

    allow user to specify what version/PR/commit of s5cmd will be used in the tests.
    user can specify either version(tag), commit hash, or the PR number of the s5cmd that are to be used in the tests.
    Two versions of the s5cmd will be used namely old and new. They can be specified by -o and -n flags, respectively.
    Though version specified by o does not have to be older than that of specified by -n flag.
    
    Default values of versions are v1.4.0 for old (-o), and v2.0.0 for new (-n).
    
    Example execution that compares performance of #456 with the v1.4.0:
    ` ./benchmark.sh -b mys5cmbuck -n 456 -o v1.4.0 -k 1829`
    
    Also note that user must provide a proper bucket which she has write/read/delete access.
    
    Co-Authored-By: boraberke <67373739+boraberke@users.noreply.github.com>

--- benchmark.sh
+++ benchmark.sh
@@ -1,23 +1,34 @@
 #! /bin/bash
 
 # constants
+start_dir=$(pwd)
 large_file_size=10G
 smaller_to_large=10000
+
+old_exec_name=olds5cmd
+new_exec_name=news5cmd
+
+# default values of options/flags
 warmup_count=2
-run_count=3
+run_count=10
 bucket=example
 key_prefix=benchmark
+new=v2.0.0
+old=v1.4.0
 
-while getopts b:k:w:r: flag
+
+# read options
+while getopts b:k:w:r:o:n: flag
 do
     case "${flag}" in
         b) bucket=${OPTARG};;
         k) key_prefix=${OPTARG};;
         w) warmup_count=${OPTARG};;
         r) run_count=${OPTARG};;
+        o) old=${OPTARG};;
+        n) new=${OPTARG};;
     esac
 done
-
 dst_prefix=s3://${bucket}/${key_prefix}
 
 
@@ -28,8 +39,38 @@ echo "All the remote files will be uploaded to the "'"'"$bucket"'"'" bucket with
 echo "Hyperfine will execute s5cmd uploads $warmup_count times to warmup, and $run_count times for measurements."
 
 
+## Git clone & build
+echo Started cloning and building the project from $old and $new.
+cd $tmp_dir
+git clone https://github.com/peak/s5cmd.git -q
+cd s5cmd
+
+## creates executables from the tags/commits/PR to parent directory
+function prepare_exec {
+if [[ "$1" =~ ^[0-9]+$ ]] 
+then
+   git pull origin pull/$1/head -q
+   echo $1 decimal $2
+elif [[ "$1" =~ ^v([0-9]+\.){2}([0-9])(-[a-z]*\.?\d?)?$ ]]
+then
+   git checkout tags/$1 -q
+   echo $1 version $2
+else
+   git checkout $1 -q
+   echo $1 commit-tag $2
+fi
+go build -o ../$2
+} 
+
+## create executables
+prepare_exec "$old" $old_exec_name
+prepare_exec "$new" $new_exec_name
+
+echo Completed cloning and building the project from $old and $new.
+cd $start_dir
+
 # create temporary files
-## one file of size 
+## one file of large size 
 large_file_dir=${tmp_dir}/${large_file_size}
 mkdir $large_file_dir
 large_file=${large_file_dir}/${large_file_size}
@@ -46,20 +87,20 @@ split -n $smaller_to_large $large_file  $small_file
 echo "Upload the large file of size $large_file_size:"
 large_first_dst=${dst_prefix}/large1/
 large_second_dst=${dst_prefix}/large2/
-large_first="s5cmd   cp ${large_file} $large_first_dst"
-large_second="./s5cmd cp ${large_file} $large_second_dst"
+large_first="$tmp_dir/$old_exec_name  cp ${large_file} $large_first_dst"
+large_second="$tmp_dir/$new_exec_name cp ${large_file} $large_second_dst"
 
-hyperfine --warmup $warmup_count --runs $run_count "$large_first" "$large_second"
+hyperfine --warmup $warmup_count --runs $run_count  -n "Old version of $old" "$large_first" -n "New version of $new" "$large_second"
 
 ### small file upload
 echo "Upload $smaller_to_large small files:"
 small_first_dst=${dst_prefix}/small1/
 small_second_dst=${dst_prefix}/small2/
 small_files="${small_file}*"
-small_first="s5cmd   cp "'"'"${small_files}"'"'" $small_first_dst"
-small_second="./s5cmd cp "'"'"${small_files}"'"'" $small_second_dst"
+small_first="$tmp_dir/$old_exec_name   cp "'"'${small_files}'"'" $small_first_dst"
+small_second="$tmp_dir/$new_exec_name  cp "'"'${small_files}'"'" $small_second_dst"
 
-hyperfine --warmup $warmup_count --runs $run_count "$small_first" "$small_second"
+hyperfine --warmup $warmup_count --runs $run_count -n "Old version of $old" "$small_first" -n "New version of $new" "$small_second"
 
 # clear the remote files --all versions!
 s5cmd --stat --log error rm "${large_first_dst}*"

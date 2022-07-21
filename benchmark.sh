1280ebae2e4b7e4fc5fb1bde7611847a95b4c3c4
boraberke <boraberke99@gmail.com>
2022-07-21 10:56:32 +0300

    Add color and fix bugs

--- benchmark.sh
+++ benchmark.sh
@@ -6,10 +6,14 @@
 readonly START_DIR=$(pwd)
 readonly GO_REQ_VERSION=1.16.0
 readonly HYPERFINE_REQ_VERSION=1.14.0
-readonly LARGE_FILE_SIZE=10G
-readonly SMALLER_TO_LARGE=10000
+readonly LARGE_FILE_SIZE=10M
+readonly SMALLER_TO_LARGE=10
 readonly OLD_EXEC_NAME=olds5cmd
 readonly NEW_EXEC_NAME=news5cmd
+readonly YELLOW='\033[1;33m'
+readonly GREEN='\033[0;32m>>'
+readonly RED='\033[0;31m'
+readonly NOCOLOR='\033[0m'
 
 # default values of options/flags
 WARMUP_COUNT=2
@@ -36,10 +40,12 @@ main() {
    make_test
 
    cleanup
+
+   echo -e "${GREEN}Benchmark completed.${NOCOLOR}"
 }
 
 read_options() {
-   while getopts b:k:w:r:o:n:f:h: flag; do
+   while getopts b:k:w:r:o:n:h:g: flag; do
       case "${flag}" in
       b) BUCKET=${OPTARG} ;;
       k) KEY_PREFIX=${OPTARG} ;;
@@ -48,7 +54,7 @@ read_options() {
       o) OLD=${OPTARG} ;;
       n) NEW=${OPTARG} ;;
       h) HYPERFINE_FLAGS=${OPTARG} ;;
-      f) GLOBAL_FLAGS=${OPTARG} ;;
+      g) GLOBAL_FLAGS=${OPTARG} ;;
       *) echo "Invalid flag(s) are used" ;;
       esac
    done
@@ -74,42 +80,57 @@ check_version() {
 create_benchmark_dir() {
    tmp_dir=$(mktemp -d -t s5cmd-benchmark-XXXXXXXXXX)
    dst_prefix=s3://${BUCKET}/${KEY_PREFIX}
-   echo "All the local temporary files will be created at $tmp_dir."
-   echo "All the remote files will be uploaded to the "'"'"$BUCKET"'"'" bucket with key prefix of "'"'"$KEY_PREFIX"'"'"."
-   echo "The created local&remote files will be deleted at the end of tests."
-   echo "Hyperfine will execute s5cmd uploads $WARMUP_COUNT times to warmup, and $RUN_COUNT times for measurements."
+   echo -e "${YELLOW}All the local temporary files will be created at $tmp_dir."
+   echo -e "All the remote files will be uploaded to the "'"'"$BUCKET"'"'" bucket with key prefix of "'"'"$KEY_PREFIX"'"'"."
+   echo -e "The created local&remote files will be deleted at the end of tests."
+   echo -e "Hyperfine will execute s5cmd uploads $WARMUP_COUNT times to warmup, and $RUN_COUNT times for measurements.${NOCOLOR}"
+}
+
+check_type(){
+   if [[ "$1" =~ ^[0-9]+$ ]]; then
+      type=PR
+   elif [[ "$1" =~ ^v([0-9]+\.){2}([0-9])(-[a-z]*\.?[0-9]?)?$ ]]; then
+      type=version
+   else
+      git checkout "$1" -q
+      type=commit
+   fi
+   echo $type
 }
 
 ## creates executables from the tags/commits/PR to parent directory
 prepare_exec() {
-   if [[ "$1" =~ ^[0-9]+$ ]]; then
-      git pull origin pull/$1/head -q
-      echo "peak:#$1 decimal $2"
-   elif [[ "$1" =~ ^v([0-9]+\.){2}([0-9])(-[a-z]*\.?\d?)?$ ]]; then
-      git checkout "tags/$1" -q
-      echo "$1 version $2"
+   if [[ "$1" = PR ]]; then
+      git fetch origin pull/$2/head -q
+   elif [[ "$1" == version ]]; then
+      git checkout "tags/$2" -q
    else
-      git checkout "$1" -q
-      echo "$1 commit-tag $2"
+      git checkout "$2" -q
    fi
-   go build -o "../$2"
+   go build -o "../$3"
 }
 
 ## git clone & build s5cmd, and user defined old and new executables.
 build_s5cmd_exec() {
-   echo "Started cloning and building the project from $OLD and $NEW."
+   echo
+   type_old=$(check_type $OLD)
+   type_new=$(check_type $NEW)
+
+   echo "Started cloning and building the project from $type_old:$OLD and $type_new:$NEW."
    cd "$tmp_dir" || exit
    git clone https://github.com/peak/s5cmd.git -q
    cd s5cmd || exit
    ## create executables
-   prepare_exec "$OLD" $OLD_EXEC_NAME
-   prepare_exec "$NEW" $NEW_EXEC_NAME
+   prepare_exec $type_old $OLD $OLD_EXEC_NAME
+   prepare_exec $type_new $NEW $NEW_EXEC_NAME
+
+   echo -e "${GREEN}Completed cloning and building the project from $type_old:$OLD and $type_new:$NEW.${NOCOLOR}"
 }
 
 create_temp_files() {
    # shellcheck disable=SC2164
    cd "$START_DIR"
-
+   echo "Creating temporary files..."
    # create temporary files
    ## one file of large size
    large_file_dir=${tmp_dir}/${LARGE_FILE_SIZE}
@@ -131,14 +152,15 @@ create_temp_files() {
    mkdir $small_file_dir
    small_file=${small_file_dir}/small
    split -a 3 -n $SMALLER_TO_LARGE $large_file $small_file
+   echo -e "${GREEN}Created temporary files in $tmp_dir${NOCOLOR}"
 }
 
 print_info() {
    echo
    if [[ "$1" == large ]]; then
-      echo "$2 the large file of size $LARGE_FILE_SIZE:"
+      echo  -e "${YELLOW}$2 the large file of size $LARGE_FILE_SIZE:${NOCOLOR}"
    elif [[ "$1" == small ]]; then
-      echo "$2 $SMALLER_TO_LARGE small files:"
+      echo  -e "${YELLOW}$2 $SMALLER_TO_LARGE small files:${NOCOLOR}"
    fi
 }
 
@@ -149,7 +171,7 @@ upload() {
    first_up="$tmp_dir/$OLD_EXEC_NAME  $GLOBAL_FLAGS cp "'"'${2}'"'" $first_dst"
    second_up="$tmp_dir/$NEW_EXEC_NAME $GLOBAL_FLAGS cp "'"'${2}'"'" $second_dst"
 
-   hyperfine "$HYPERFINE_FLAGS" --warmup $WARMUP_COUNT --runs $RUN_COUNT "$first_up" "$second_up"
+   hyperfine $HYPERFINE_FLAGS --warmup $WARMUP_COUNT --runs $RUN_COUNT -n $type_old:$OLD "$first_up" -n $type_new:$NEW "$second_up"
 }
 
 download() {
@@ -161,7 +183,7 @@ download() {
    first_dl="$tmp_dir/$OLD_EXEC_NAME  $GLOBAL_FLAGS cp "'"'$first_dst'"'" $2/"
    second_dl="$tmp_dir/$NEW_EXEC_NAME $GLOBAL_FLAGS cp "'"'$second_dst'"'" $2/"
 
-   hyperfine "$HYPERFINE_FLAGS" --warmup $WARMUP_COUNT --runs $RUN_COUNT "$first_dl" "$second_dl"
+   hyperfine $HYPERFINE_FLAGS --warmup $WARMUP_COUNT --runs $RUN_COUNT -n $type_old:$OLD "$first_dl" -n $type_new:$NEW "$second_dl"
 }
 
 remove() {
@@ -174,14 +196,14 @@ remove() {
    second_rm="$tmp_dir/$NEW_EXEC_NAME $GLOBAL_FLAGS rm ${second_dst}"
 
    # one can delete files once! So --warmup 0 --runs 1!
-   hyperfine "$HYPERFINE_FLAGS" --warmup 0 --runs 1 "$first_rm" "$second_rm"
+   hyperfine $HYPERFINE_FLAGS --warmup 0 --runs 1 -n $type_old:$OLD "$first_rm" -n $type_new:$NEW "$second_rm"
 }
 
 ## Make the tests!
 make_test() {
 
    upload large $large_file
-   upload small ${small_file}*
+   upload small ${small_file}"*"
 
    download large $large_file_dir
    download small $small_file_dir
@@ -194,8 +216,9 @@ make_test() {
 cleanup() {
    # clear the temporary directories and files in local
    echo
-   echo "Delete the temporary directories and files in local from $tmp_dir"
+   echo "Deleting the temporary directories and files in local from $tmp_dir..."
    rm -rf "$tmp_dir"
+   echo -e "${GREEN}Deleted the temporary directories and files.${NOCOLOR}"
 }
 
 main "$@"

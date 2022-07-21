240e7259ed58c58982425a7d18d7b5339f1f774a
boraberke <boraberke99@gmail.com>
2022-07-21 15:19:28 +0300

    Add Markdown Bench Results

--- benchmark.sh
+++ benchmark.sh
@@ -6,14 +6,15 @@
 readonly START_DIR=$(pwd)
 readonly GO_REQ_VERSION=1.16.0
 readonly HYPERFINE_REQ_VERSION=1.14.0
-readonly LARGE_FILE_SIZE=10M
-readonly SMALLER_TO_LARGE=10
+readonly LARGE_FILE_SIZE=100M
+readonly SMALLER_TO_LARGE=100
 readonly OLD_EXEC_NAME=olds5cmd
 readonly NEW_EXEC_NAME=news5cmd
 readonly YELLOW='\033[1;33m'
 readonly GREEN='\033[0;32m>>'
 readonly RED='\033[0;31m'
 readonly NOCOLOR='\033[0m'
+readonly BENCH_RESULT=bench_results.md
 
 # default values of options/flags
 WARMUP_COUNT=2
@@ -26,199 +27,220 @@ GLOBAL_FLAGS=""
 HYPERFINE_FLAGS=""
 
 main() {
-   read_options "$@"
+	read_options "$@"
 
-   check_version "go version" $GO_REQ_VERSION
-   check_version "hyperfine --version" $HYPERFINE_REQ_VERSION
+	check_version "go version" $GO_REQ_VERSION
+	check_version "hyperfine --version" $HYPERFINE_REQ_VERSION
 
-   create_benchmark_dir
+	create_benchmark_dir
 
-   build_s5cmd_exec
+	build_s5cmd_exec
 
-   create_temp_files
+	create_temp_files
 
-   make_test
+	make_test
 
-   cleanup
+	cleanup
 
-   echo -e "${GREEN}Benchmark completed.${NOCOLOR}"
+	echo -e "${GREEN}Benchmark completed. Benchmark results are saved in $START_DIR/$BENCH_RESULT ${NOCOLOR}"
 }
 
 read_options() {
-   while getopts b:k:w:r:o:n:h:g: flag; do
-      case "${flag}" in
-      b) BUCKET=${OPTARG} ;;
-      k) KEY_PREFIX=${OPTARG} ;;
-      w) WARMUP_COUNT=${OPTARG} ;;
-      r) RUN_COUNT=${OPTARG} ;;
-      o) OLD=${OPTARG} ;;
-      n) NEW=${OPTARG} ;;
-      h) HYPERFINE_FLAGS=${OPTARG} ;;
-      g) GLOBAL_FLAGS=${OPTARG} ;;
-      *) echo "Invalid flag(s) are used" ;;
-      esac
-   done
-   readonly BUCKET
-   readonly KEY_PREFIX
-   readonly WARMUP_COUNT
-   readonly RUN_COUNT
-   readonly OLD
-   readonly NEW
+	while getopts b:k:w:r:o:n:h:g: flag; do
+		case "${flag}" in
+		b) BUCKET=${OPTARG} ;;
+		k) KEY_PREFIX=${OPTARG} ;;
+		w) WARMUP_COUNT=${OPTARG} ;;
+		r) RUN_COUNT=${OPTARG} ;;
+		o) OLD=${OPTARG} ;;
+		n) NEW=${OPTARG} ;;
+		h) HYPERFINE_FLAGS=${OPTARG} ;;
+		g) GLOBAL_FLAGS=${OPTARG} ;;
+		*) echo "Invalid flag(s) are used" ;;
+		esac
+	done
+	readonly BUCKET
+	readonly KEY_PREFIX
+	readonly WARMUP_COUNT
+	readonly RUN_COUNT
+	readonly OLD
+	readonly NEW
 }
 
 ## check version of a program and exit if it is less than required version
 check_version() {
-   currentver=$(grep -Eo '([0-9]+\.){2}([0-9])' <<<$($1))
-   requiredver=$2
-   if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" != "$requiredver" ]; then
-      echo "Less than ${requiredver}"
-      exit
-   fi
+	currentver=$(grep -Eo '([0-9]+\.){2}([0-9])' <<<$($1))
+	requiredver=$2
+	if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" != "$requiredver" ]; then
+		echo "Less than ${requiredver}"
+		exit
+	fi
 }
 
 ## create the temporary benchmark directory
 create_benchmark_dir() {
-   tmp_dir=$(mktemp -d -t s5cmd-benchmark-XXXXXXXXXX)
-   dst_prefix=s3://${BUCKET}/${KEY_PREFIX}
-   echo -e "${YELLOW}All the local temporary files will be created at $tmp_dir."
-   echo -e "All the remote files will be uploaded to the "'"'"$BUCKET"'"'" bucket with key prefix of "'"'"$KEY_PREFIX"'"'"."
-   echo -e "The created local&remote files will be deleted at the end of tests."
-   echo -e "Hyperfine will execute s5cmd uploads $WARMUP_COUNT times to warmup, and $RUN_COUNT times for measurements.${NOCOLOR}"
+	tmp_dir=$(mktemp -d -t s5cmd-benchmark-XXXXXXXXXX)
+	dst_prefix=s3://${BUCKET}/${KEY_PREFIX}
+	echo -e "${YELLOW}All the local temporary files will be created at $tmp_dir."
+	echo -e "All the remote files will be uploaded to the "'"'"$BUCKET"'"'" bucket with key prefix of "'"'"$KEY_PREFIX"'"'"."
+	echo -e "The created local&remote files will be deleted at the end of tests."
+	echo -e "Hyperfine will execute s5cmd uploads $WARMUP_COUNT times to warmup, and $RUN_COUNT times for measurements.${NOCOLOR}"
 }
 
-check_type(){
-   if [[ "$1" =~ ^[0-9]+$ ]]; then
-      type=PR
-   elif [[ "$1" =~ ^v([0-9]+\.){2}([0-9])(-[a-z]*\.?[0-9]?)?$ ]]; then
-      type=version
-   else
-      git checkout "$1" -q
-      type=commit
-   fi
-   echo $type
+check_type() {
+	if [[ "$1" =~ ^[0-9]+$ ]]; then
+		type=PR
+	elif [[ "$1" =~ ^v([0-9]+\.){2}([0-9])(-[a-z]*\.?[0-9]?)?$ ]]; then
+		type=version
+	else
+		git checkout "$1" -q
+		type=commit
+	fi
+	echo $type
 }
 
 ## creates executables from the tags/commits/PR to parent directory
 prepare_exec() {
-   if [[ "$1" = PR ]]; then
-      git fetch origin pull/$2/head -q
-   elif [[ "$1" == version ]]; then
-      git checkout "tags/$2" -q
-   else
-      git checkout "$2" -q
-   fi
-   go build -o "../$3"
+	if [[ "$1" = PR ]]; then
+		git fetch origin pull/$2/head -q
+	elif [[ "$1" == version ]]; then
+		git checkout "tags/$2" -q
+	else
+		git checkout "$2" -q
+	fi
+	go build -o "../$3"
 }
 
 ## git clone & build s5cmd, and user defined old and new executables.
 build_s5cmd_exec() {
-   echo
-   type_old=$(check_type $OLD)
-   type_new=$(check_type $NEW)
+	echo
+	type_old=$(check_type $OLD)
+	type_new=$(check_type $NEW)
 
-   echo "Started cloning and building the project from $type_old:$OLD and $type_new:$NEW."
-   cd "$tmp_dir" || exit
-   git clone https://github.com/peak/s5cmd.git -q
-   cd s5cmd || exit
-   ## create executables
-   prepare_exec $type_old $OLD $OLD_EXEC_NAME
-   prepare_exec $type_new $NEW $NEW_EXEC_NAME
+	echo "Started cloning and building the project from $type_old:$OLD and $type_new:$NEW."
+	cd "$tmp_dir" || exit
+	git clone https://github.com/peak/s5cmd.git -q
+	cd s5cmd || exit
+	## create executables
+	prepare_exec $type_old $OLD $OLD_EXEC_NAME
+	prepare_exec $type_new $NEW $NEW_EXEC_NAME
 
-   echo -e "${GREEN}Completed cloning and building the project from $type_old:$OLD and $type_new:$NEW.${NOCOLOR}"
+	echo -e "${GREEN}Completed cloning and building the project from $type_old:$OLD and $type_new:$NEW.${NOCOLOR}"
 }
 
 create_temp_files() {
-   # shellcheck disable=SC2164
-   cd "$START_DIR"
-   echo "Creating temporary files..."
-   # create temporary files
-   ## one file of large size
-   large_file_dir=${tmp_dir}/${LARGE_FILE_SIZE}
-   mkdir "$large_file_dir"
-   large_file=${large_file_dir}/${LARGE_FILE_SIZE}
-   ### create the large file
-   case "$OSTYPE" in
-   darwin*) mkfile -n $LARGE_FILE_SIZE ${large_file} ;;
-   linux*) truncate --size $LARGE_FILE_SIZE ${large_file} ;;
-   msys*) echo "This script cannot run in Windows" && exit ;;
-   cygwin*) echo "This script cannot run in Windows" && exit ;;
-   *) truncate --size $LARGE_FILE_SIZE ${large_file} ;;
-      # one should not use Windows. I don't want to manually parse $LARGE_FILE_SIZE!
-      # fsutil file createnew (?) only accepts numbers, without "humanized" suffices!
-   esac
+	# shellcheck disable=SC2164
+	cd "$START_DIR"
+	echo "Creating temporary files..."
+	# create temporary files
+	## one file of large size
+	large_file_dir=${tmp_dir}/${LARGE_FILE_SIZE}
+	mkdir "$large_file_dir"
+	large_file=${large_file_dir}/${LARGE_FILE_SIZE}
+	### create the large file
+	case "$OSTYPE" in
+	darwin*) mkfile -n $LARGE_FILE_SIZE ${large_file} ;;
+	linux*) truncate --size $LARGE_FILE_SIZE ${large_file} ;;
+	msys*) echo "This script cannot run in Windows" && exit ;;
+	cygwin*) echo "This script cannot run in Windows" && exit ;;
+	*) truncate --size $LARGE_FILE_SIZE ${large_file} ;;
+		# one should not use Windows. I don't want to manually parse $LARGE_FILE_SIZE!
+		# fsutil file createnew (?) only accepts numbers, without "humanized" suffices!
+	esac
 
-   ## create smaller files from the large file
-   small_file_dir=${tmp_dir}/small
-   mkdir $small_file_dir
-   small_file=${small_file_dir}/small
-   split -a 3 -n $SMALLER_TO_LARGE $large_file $small_file
-   echo -e "${GREEN}Created temporary files in $tmp_dir${NOCOLOR}"
+	## create smaller files from the large file
+	small_file_dir=${tmp_dir}/small
+	mkdir $small_file_dir
+	small_file=${small_file_dir}/small
+	split -a 3 -n $SMALLER_TO_LARGE $large_file $small_file
+	echo -e "${GREEN}Created temporary files in $tmp_dir${NOCOLOR}"
 }
 
 print_info() {
-   echo
-   if [[ "$1" == large ]]; then
-      echo  -e "${YELLOW}$2 the large file of size $LARGE_FILE_SIZE:${NOCOLOR}"
-   elif [[ "$1" == small ]]; then
-      echo  -e "${YELLOW}$2 $SMALLER_TO_LARGE small files:${NOCOLOR}"
-   fi
+	echo
+	if [[ "$1" == large ]]; then
+		echo -e "${YELLOW}$2 the large file of size $LARGE_FILE_SIZE:${NOCOLOR}"
+	elif [[ "$1" == small ]]; then
+		echo -e "${YELLOW}$2 $SMALLER_TO_LARGE small files:${NOCOLOR}"
+	fi
+}
+
+save_results() {
+	sed -i -e "1,2d" tmp.md
+	sed -i -e "s/^/|$2_$1/" tmp.md
+	cat tmp.md >>$BENCH_RESULT
 }
 
 upload() {
-   print_info $1 Upload
-   first_dst=${dst_prefix}/${1}1/
-   second_dst=${dst_prefix}/${1}2/
-   first_up="$tmp_dir/$OLD_EXEC_NAME  $GLOBAL_FLAGS cp "'"'${2}'"'" $first_dst"
-   second_up="$tmp_dir/$NEW_EXEC_NAME $GLOBAL_FLAGS cp "'"'${2}'"'" $second_dst"
+	print_info $1 Upload
+	first_dst=${dst_prefix}/${1}1/
+	second_dst=${dst_prefix}/${1}2/
+	first_up="$tmp_dir/$OLD_EXEC_NAME  $GLOBAL_FLAGS cp "'"'${2}'"'" $first_dst"
+	second_up="$tmp_dir/$NEW_EXEC_NAME $GLOBAL_FLAGS cp "'"'${2}'"'" $second_dst"
 
-   hyperfine $HYPERFINE_FLAGS --warmup $WARMUP_COUNT --runs $RUN_COUNT -n $type_old:$OLD "$first_up" -n $type_new:$NEW "$second_up"
+	hyperfine --export-markdown tmp.md $HYPERFINE_FLAGS --warmup $WARMUP_COUNT --runs $RUN_COUNT -n $type_old:$OLD "$first_up" -n $type_new:$NEW "$second_up"
+
+	save_results $1 Upload
 }
 
 download() {
-   # We can download to the same directory that we uploaded the files from, and we will.
-   # Both of them writes to the same directory, but, for now, I don't care.
-   print_info $1 Download
-   first_dst=${dst_prefix}/${1}1/*
-   second_dst=${dst_prefix}/${1}2/*
-   first_dl="$tmp_dir/$OLD_EXEC_NAME  $GLOBAL_FLAGS cp "'"'$first_dst'"'" $2/"
-   second_dl="$tmp_dir/$NEW_EXEC_NAME $GLOBAL_FLAGS cp "'"'$second_dst'"'" $2/"
+	# We can download to the same directory that we uploaded the files from, and we will.
+	# Both of them writes to the same directory, but, for now, I don't care.
+	print_info $1 Download
+	first_dst=${dst_prefix}/${1}1/*
+	second_dst=${dst_prefix}/${1}2/*
+	first_dl="$tmp_dir/$OLD_EXEC_NAME  $GLOBAL_FLAGS cp "'"'$first_dst'"'" $2/"
+	second_dl="$tmp_dir/$NEW_EXEC_NAME $GLOBAL_FLAGS cp "'"'$second_dst'"'" $2/"
 
-   hyperfine $HYPERFINE_FLAGS --warmup $WARMUP_COUNT --runs $RUN_COUNT -n $type_old:$OLD "$first_dl" -n $type_new:$NEW "$second_dl"
+	hyperfine --export-markdown tmp.md $HYPERFINE_FLAGS --warmup $WARMUP_COUNT --runs $RUN_COUNT -n $type_old:$OLD "$first_dl" -n $type_new:$NEW "$second_dl"
+
+	save_results $1 Download
 }
 
 remove() {
-   ### clear the remote files --iff bucket is unversioned, otherwise just puts
-   ### delete marker(s), sorry about that!
-   print_info $1 Remove
-   first_dst=${dst_prefix}/${1}1/*
-   second_dst=${dst_prefix}/${1}2/*
-   first_rm="$tmp_dir/$OLD_EXEC_NAME $GLOBAL_FLAGS rm ${first_dst}"
-   second_rm="$tmp_dir/$NEW_EXEC_NAME $GLOBAL_FLAGS rm ${second_dst}"
+	### clear the remote files --iff bucket is unversioned, otherwise just puts
+	### delete marker(s), sorry about that!
+	print_info $1 Remove
+	first_dst=${dst_prefix}/${1}1/*
+	second_dst=${dst_prefix}/${1}2/*
+	first_rm="$tmp_dir/$OLD_EXEC_NAME $GLOBAL_FLAGS rm ${first_dst}"
+	second_rm="$tmp_dir/$NEW_EXEC_NAME $GLOBAL_FLAGS rm ${second_dst}"
 
-   # one can delete files once! So --warmup 0 --runs 1!
-   hyperfine $HYPERFINE_FLAGS --warmup 0 --runs 1 -n $type_old:$OLD "$first_rm" -n $type_new:$NEW "$second_rm"
+	# one can delete files once! So --warmup 0 --runs 1!
+	hyperfine --export-markdown tmp.md $HYPERFINE_FLAGS --warmup 0 --runs 1 -n $type_old:$OLD "$first_rm" -n $type_new:$NEW "$second_rm"
+
+	save_results $1 Remove
+}
+init_bench_results() {
+	header="|Scenario| Command | Mean [ms] | Min [ms] | Max [ms] | Relative |"
+	header2="|:---|:---|---:|---:|---:|---:|"
+
+	touch $BENCH_RESULT
+	echo "$header"$'\n'"$header2" >$BENCH_RESULT
 }
 
 ## Make the tests!
 make_test() {
 
-   upload large $large_file
-   upload small ${small_file}"*"
+	init_bench_results
 
-   download large $large_file_dir
-   download small $small_file_dir
+	upload large $large_file
+	upload small ${small_file}"*"
 
-   remove large
-   remove small
+	download large $large_file_dir
+	download small $small_file_dir
 
+	remove large
+	remove small
 }
 
 cleanup() {
-   # clear the temporary directories and files in local
-   echo
-   echo "Deleting the temporary directories and files in local from $tmp_dir..."
-   rm -rf "$tmp_dir"
-   echo -e "${GREEN}Deleted the temporary directories and files.${NOCOLOR}"
+	# clear the temporary directories and files in local
+	echo
+	echo "Deleting the temporary directories and files in local from $tmp_dir..."
+	rm tmp.md*
+	rm -rf "$tmp_dir"
+	echo -e "${GREEN}Deleted the temporary directories and files.${NOCOLOR}"
 }
 
 main "$@"

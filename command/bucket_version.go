193a94787903713fe104d2cf52b6c22fa6de39fe
Justin Manley <justin.manley@sensat.co.uk>
2023-06-19 14:02:02 +0100

    Add v2 to import paths in bucket_version.go.

--- command/bucket_version.go
+++ command/bucket_version.go
@@ -7,10 +7,10 @@ import (
 
 	"github.com/urfave/cli/v2"
 
-	"github.com/peak/s5cmd/log"
-	"github.com/peak/s5cmd/storage"
-	"github.com/peak/s5cmd/storage/url"
-	"github.com/peak/s5cmd/strutil"
+	"github.com/peak/s5cmd/v2/log"
+	"github.com/peak/s5cmd/v2/storage"
+	"github.com/peak/s5cmd/v2/storage/url"
+	"github.com/peak/s5cmd/v2/strutil"
 )
 
 var bucketVersionHelpTemplate = `Name:

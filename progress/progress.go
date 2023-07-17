fa20fd0ed92dc965da4f60d269b24fdfd3691ce8
Ahmet Hakan Beşel <ahmethakan.besel@gmail.com>
2023-07-17 14:18:24 +0300

    progress: update progress bar appearance

--- progress/progress.go
+++ progress/progress.go
@@ -8,16 +8,15 @@ import (
 )
 
 type CommandProgress struct {
-	// task status
 	totalObjects     int64
 	completedObjects int64
 	totalBytes       int64
 	completedBytes   int64
-	mutex            sync.RWMutex
+	mu               sync.RWMutex
 	progressbar      *pb.ProgressBar
 }
 
-const progressbarTemplate = `{{ string . "taskStatus" }} {{percent .}} {{ bar .  | green}} {{counters .}} {{speed . "(%s/s)" }} {{rtime . "%s left"}}`
+const progressbarTemplate = `{{percent . | green}} {{bar . " " "━" "─" "─" " " | green}} {{counters . | green}} {{speed . "(%s/s)" | red}} {{rtime . "%s left" | blue}} {{ string . "objects" | yellow}}`
 
 func (cp *CommandProgress) InitializeProgressBar() {
 	cp.progressbar = pb.New64(0)
@@ -25,7 +24,7 @@ func (cp *CommandProgress) InitializeProgressBar() {
 	cp.progressbar.Set(pb.SIBytesPrefix, true)
 	cp.progressbar.SetWidth(128)
 	cp.progressbar.SetTemplateString(progressbarTemplate)
-	cp.progressbar.Set("taskStatus", fmt.Sprintf("%d/%d items are completed.", 0, 0))
+	cp.progressbar.Set("objects", fmt.Sprintf("(%d/%d)", 0, 0))
 	cp.progressbar.Start()
 }
 
@@ -34,36 +33,36 @@ func (cp *CommandProgress) Finish() {
 }
 
 func (cp *CommandProgress) IncrementCompletedObjects() {
-	cp.mutex.Lock()
-	defer cp.mutex.Unlock()
+	cp.mu.Lock()
+	defer cp.mu.Unlock()
 	cp.completedObjects += 1
-	cp.progressbar.Set("taskStatus", fmt.Sprintf("%d/%d items are completed.", cp.completedObjects, cp.totalObjects))
+	cp.progressbar.Set("objects", fmt.Sprintf("(%d/%d)", cp.completedObjects, cp.totalObjects))
 }
 
 func (cp *CommandProgress) IncrementTotalObjects() {
-	cp.mutex.Lock()
-	defer cp.mutex.Unlock()
+	cp.mu.Lock()
+	defer cp.mu.Unlock()
 	cp.totalObjects += 1
-	cp.progressbar.Set("taskStatus", fmt.Sprintf("%d/%d items are completed.", cp.completedObjects, cp.totalObjects))
+	cp.progressbar.Set("objects", fmt.Sprintf("(%d/%d)", cp.completedObjects, cp.totalObjects))
 }
 
 func (cp *CommandProgress) AddCompletedBytesInt64(bytes int64) {
-	cp.mutex.Lock()
-	defer cp.mutex.Unlock()
+	cp.mu.Lock()
+	defer cp.mu.Unlock()
 	cp.completedBytes += bytes
 	cp.progressbar.Add64(bytes)
 }
 
 func (cp *CommandProgress) AddCompletedBytes(bytes int) {
-	cp.mutex.Lock()
-	defer cp.mutex.Unlock()
+	cp.mu.Lock()
+	defer cp.mu.Unlock()
 	cp.completedBytes += int64(bytes)
 	cp.progressbar.Add(bytes)
 }
 
 func (cp *CommandProgress) AddTotalBytes(bytes int64) {
-	cp.mutex.Lock()
-	defer cp.mutex.Unlock()
+	cp.mu.Lock()
+	defer cp.mu.Unlock()
 	cp.totalBytes += bytes
 	cp.progressbar.SetTotal(cp.totalBytes)
 }

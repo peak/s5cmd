d2d4e1449fe9d145be6299b2ad43a59f243231a4
Deniz Sürmeli <denizsurmeli@icloud.com>
2023-07-18 11:24:35 +0300

    chore: remove debug lines

--- buffer/llbuffer.go
+++ buffer/llbuffer.go
@@ -2,11 +2,8 @@ package buffer
 
 import (
 	"container/list"
-	"fmt"
 	"io"
 	"sync"
-
-	"github.com/peak/s5cmd/v2/log"
 )
 
 type FileChunk struct {
@@ -37,23 +34,12 @@ func NewOrderedBuffer(expectedBytes int64, maxSize int, w io.Writer) *OrderedBuf
 	}
 }
 
-func debugMessage(s string) {
-	return
-	fmt.Printf("%v\n", log.DebugMessage{
-		Operation: "Get",
-		Command:   "cat",
-		Err:       s,
-	})
-}
-
 func (ob *OrderedBuffer) WriteAt(p []byte, offset int64) (int, error) {
 	ob.mu.Lock()
 	defer ob.mu.Unlock()
-	debugMessage(fmt.Sprintf("WriteAt with offset: %d, len:%d", offset, len(p)))
 	if ob.list.Front() == nil {
 		// if the queue is empty and the chunk is writeable, push it without queueing
 		if ob.written == offset {
-			debugMessage(fmt.Sprintf("WriteAt bypass-write with offset: %d, len:%d", offset, len(p)))
 			ob.w.Write(p)
 			ob.written += int64(len(p))
 
@@ -70,10 +56,8 @@ func (ob *OrderedBuffer) WriteAt(p []byte, offset int64) (int, error) {
 	for e := ob.list.Front(); e != nil; e = e.Next() {
 		v, ok := e.Value.(*FileChunk)
 		if !ok {
-			debugMessage("error while casting the node\n")
 		}
 		if offset < v.Start {
-			debugMessage(fmt.Sprintf("\tWriteAt insert before %d,  offset: %d, len:%d", v.Start, offset, len(p)))
 			ob.list.InsertBefore(&FileChunk{
 				Start:   offset,
 				Content: p,
@@ -87,21 +71,18 @@ func (ob *OrderedBuffer) WriteAt(p []byte, offset int64) (int, error) {
 			Start:   offset,
 			Content: p,
 		})
-		debugMessage(fmt.Sprintf("\tWriteAt push back offset: %d, len:%d", offset, len(p)))
 	}
 	ob.Queued++
-	debugMessage(fmt.Sprintf("\tWriteAt Total Queued after writes: %d", ob.Queued))
 	removeList := make([]*list.Element, 0)
 	for e := ob.list.Front(); e != nil; e = e.Next() {
 		v, ok := e.Value.(*FileChunk)
 		if !ok {
-			debugMessage("error while casting the node\n")
+			// NOTE: should we handle this ?
+			continue
 		}
 		if v.Start == ob.written {
-			debugMessage(fmt.Sprintf("\tWriteAt read offset %d, len:%d", v.Start, len(v.Content)))
 			n, err := ob.w.Write(v.Content)
 			if err != nil {
-				debugMessage("error while writing the next chunk to the writer")
 				return n, err
 			}
 			removeList = append(removeList, e)
@@ -111,7 +92,6 @@ func (ob *OrderedBuffer) WriteAt(p []byte, offset int64) (int, error) {
 			break
 		}
 	}
-	debugMessage(fmt.Sprintf("\tWriteAt Total Queued after reads: %d", ob.Queued))
 	for _, e := range removeList {
 		ob.list.Remove(e)
 	}

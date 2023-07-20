af738d43b2d0d2ee4f49389e9476958191dcd978
Ahmet Hakan <ahmethakan.besel@gmail.com>
2023-07-20 23:25:22 +0300

    storage/fs: update comments

--- storage/fs.go
+++ storage/fs.go
@@ -246,19 +246,16 @@ func (f *Filesystem) Rename(file *os.File, newpath string) error {
 
 	err := os.Rename(file.Name(), newpath)
 	if err != nil {
-		// Read the content from the source file
 		data, err := os.ReadFile(file.Name())
 		if err != nil {
 			return err
 		}
 
-		// Write the content to the destination file
 		err = os.WriteFile(newpath, data, 0644)
 		if err != nil {
 			return err
 		}
-
-		// Remove the source file after successfully copying its content
+		
 		if err := os.Remove(file.Name()); err != nil {
 			return err
 		}

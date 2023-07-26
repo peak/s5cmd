873c42b540b90576db907bb53afc00bf8db9789c
Ahmet Hakan Beşel <ahmethakan.besel@gmail.com>
2023-07-26 10:55:29 +0300

    command/cp: add an unit test for `shouldCopyObject`

--- command/cp_test.go
+++ command/cp_test.go
@@ -3,8 +3,11 @@ package command
 import (
 	"io"
 	"os"
+	"reflect"
 	"testing"
 
+	"github.com/peak/s5cmd/v2/storage"
+	"github.com/peak/s5cmd/v2/storage/url"
 	"gotest.tools/v3/assert"
 )
 
@@ -75,5 +78,74 @@ func TestGuessContentType(t *testing.T) {
 
 		f.Close()
 		os.Remove(f.Name())
+	}
+}
+
+func TestShouldCopyObject(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		excludePatterns []string
+		includePatterns []string
+		objects         []string
+		filteredObjects []string
+	}{
+		{
+			excludePatterns: []string{"*.txt", "*.log"},
+			includePatterns: []string{"file-*.doc"},
+			objects:         []string{"document.txt", "file-2.log", "file-1.doc", "image.png"},
+			filteredObjects: []string{"file-1.doc"},
+		},
+		{
+			excludePatterns: []string{"secret-*"},
+			includePatterns: []string{"*.txt", "*.log"},
+			objects:         []string{"secret-passwords.txt", "file-1.txt", "file-2.txt", "image.png"},
+			filteredObjects: []string{"file-1.txt", "file-2.txt"},
+		},
+		{
+			excludePatterns: []string{},
+			includePatterns: []string{"*.png"},
+			objects:         []string{"secret-passwords.txt", "file-1.txt", "file-2.txt", "image.png"},
+			filteredObjects: []string{"image.png"},
+		},
+		{
+			excludePatterns: []string{"file*"},
+			includePatterns: []string{},
+			objects:         []string{"readme.md", "file-1.txt", "file-2.txt", "image.png"},
+			filteredObjects: []string{"readme.md", "image.png"},
+		},
+		{
+			excludePatterns: []string{"file*"},
+			includePatterns: []string{"*txt"},
+			objects:         []string{"readme.txt", "file-1.txt", "file-2.txt", "license.txt"},
+			filteredObjects: []string{"readme.txt", "license.txt"},
+		},
+		{
+			excludePatterns: []string{"*tmp", "*.txt"},
+			includePatterns: []string{"*png", "*.doc*"},
+			objects:         []string{"readme.txt", "license.txt", "cache.tmp", "image.png", "eula.doc", "eula.docx", "personaldoc"},
+			filteredObjects: []string{"image.png", "eula.doc", "eula.docx"},
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc
+		excludeRegex, err := createRegexFromWildcard(tc.excludePatterns)
+		if err != nil {
+			t.Error(err)
+		}
+		includeRegex, err := createRegexFromWildcard(tc.includePatterns)
+		if err != nil {
+			t.Error(err)
+		}
+		var filteredObjects []string
+		cp := Copy{excludePatterns: excludeRegex, includePatterns: includeRegex}
+		cp.src = &url.URL{Prefix: ""}
+		for _, object := range tc.objects {
+			if cp.shouldCopyObject(&storage.Object{URL: &url.URL{Path: object}}, false) {
+				filteredObjects = append(filteredObjects, object)
+			}
+		}
+		assert.Equal(t, reflect.DeepEqual(tc.filteredObjects, filteredObjects), true)
 	}
 }

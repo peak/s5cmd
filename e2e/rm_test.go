de9fccd3b357afc1d75e70b2d91397431df90c66
Ahmet Hakan Beşel <ahmethakan.besel@gmail.com>
2023-07-26 16:06:05 +0300

    e2e/rm: test skip special files

--- e2e/rm_test.go
+++ e2e/rm_test.go
@@ -2,6 +2,7 @@ package e2e
 
 import (
 	"fmt"
+	"net"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -1303,37 +1304,26 @@ func TestRemoveByVersionID(t *testing.T) {
 	assert.Assert(t, result.Stdout() == "")
 }
 
-func TestRemovingSpecialFile(t *testing.T) {
+func TestRemovingSocketFile(t *testing.T) {
 	t.Parallel()
-
-	s3client, s5cmd := setup(t)
-	bucket := s3BucketFromTestName(t)
-	createBucket(t, s3client, bucket)
-
-	const (
-		filename = "special_file"
-		content  = "preserved content"
-	)
-
-	workdir := fs.NewDir(t, t.Name(), fs.WithFile(filename, content, fs.WithMode(os.ModeSocket)))
+	_, s5cmd := setup(t)
+	workdir := fs.NewDir(t, t.Name())
 	defer workdir.Remove()
-
-	cmd := s5cmd("rm", filename)
+	sockaddr := workdir.Path() + "/s5cmd.sock"
+	ln, _ := net.Listen("unix", sockaddr)
+	t.Cleanup(func() {
+		ln.Close()
+		os.Remove(sockaddr)
+	})
+	cmd := s5cmd("rm", sockaddr)
 	result := icmd.RunCmd(cmd, withWorkingDir(workdir))
 
-	// assert permissiong denied while opening the file
-	assertLines(t, result.Stderr(), map[int]compareFunc{
-		0: contains("permission denied"),
-	})
+	// assert no error
+	assertLines(t, result.Stderr(), map[int]compareFunc{})
 
-	// assert logs are empty
+	// assert logs are empty (no remove)
 	assertLines(t, result.Stdout(), map[int]compareFunc{})
 
 	// assert exit code
-	result.Assert(t, icmd.Expected{ExitCode: 1})
-
-	// assert object is not uploaded
-	assertLines(t, ensureS3Object(s3client, bucket, filename, content).Error(), map[int]compareFunc{
-		0: contains("no such key"),
-	})
+	result.Assert(t, icmd.Success)
 }

b9412dfd580a71298f1a596a9be2583490895e2d
Tarık Buğra Özyurt <74174778+tarikozyurtt@users.noreply.github.com>
2024-07-05 14:21:24 +0300

    Update du_test.go

--- e2e/du_test.go
+++ e2e/du_test.go
@@ -312,7 +312,6 @@ func TestDiskUsageEmptyBucket(t *testing.T) {
 	cmd := s5cmd("du", "s3://"+bucket)
 	result := icmd.RunCmd(cmd)
 
-
 	result.Assert(t, icmd.Success)
 
 	assertLines(t, result.Stdout(), map[int]compareFunc{
